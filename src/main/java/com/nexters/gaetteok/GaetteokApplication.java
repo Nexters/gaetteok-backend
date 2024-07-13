f818e9ab50a68b9c5741d8cf872ae0cd4dd9c8c0
YIHYUN HA <hihyun16@gmail.com>
2024-07-13 15:00:57 +0900

    Init project

---
+++ src/main/java/com/nexters/gaetteok/GaetteokApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.gaetteok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaetteokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaetteokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/gaetteok/GaetteokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.gaetteok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaetteokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
