4fe742a6992ad79c55fb4015f13eedb2ec51a36d
Harry <43809168+minkukjo@users.noreply.github.com>
2024-07-27 23:43:46 +0900

    Add multimodule (#4)
    * Add multimodule
    
    * Fix dockerfile and add prod db
    
    * Add files
    
    ---------
    
    Co-authored-by: 조민국 <minkuk.j@navercorp.com>

--- api/src/main/java/com/nexters/gaetteok/ApiApplication.java
+++ api/src/main/java/com/nexters/gaetteok/ApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GaetteokApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GaetteokApplication.class, args);
-	}
-
+public class ApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 }
