282f09dcd95e9bd1a29c803a103ba4057f15f4f9
minkukjo <strike0115@naver.com>
2024-08-21 01:00:40 +0900

    Patch walk time

--- api/src/main/java/com/nexters/gaetteok/walklog/presentation/request/PatchWalkLogRequest.java
+++ api/src/main/java/com/nexters/gaetteok/walklog/presentation/request/PatchWalkLogRequest.java
@@ -9,15 +9,19 @@ import lombok.ToString;
 @ToString
 public class PatchWalkLogRequest {
 
-    @Schema(description = "산책 기록 수정 제목", example = "산책 기록 수정 제목")
+    @Schema(description = "산책 기록 수정 제목", example = "오늘 산책한 썰 푼다.")
     private String title;
 
-    @Schema(description = "산책 기록 수정 내용", example = "산책 기록 수정 내용")
+    @Schema(description = "산책 기록 수정 내용", example = "오늘 산책 오졌다 ㅇㅈ?")
     private String content;
+
+    @Schema(description = "산책 시간 수정 내용", example = "WITHIN_20_MINUTES")
+    private WalkTime walkTime;
 
     public PatchWalkLogRequest(String title, String content, WalkTime walkTime) {
         this.title = title;
         this.content = content;
+        this.walkTime = walkTime;
     }
 
 }
