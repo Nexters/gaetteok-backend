e20258449887ffc9f9852dfc722a349ebd581891
minkukjo <strike0115@naver.com>
2024-08-22 00:34:11 +0900

    Add method signature

--- api/src/main/java/com/nexters/gaetteok/notification/presentation/PushNotificationController.java
+++ api/src/main/java/com/nexters/gaetteok/notification/presentation/PushNotificationController.java
@@ -2,6 +2,7 @@ package com.nexters.gaetteok.notification.presentation;
 
 import com.nexters.gaetteok.firebase.service.PushNotificationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,8 @@ public class PushNotificationController {
     private final PushNotificationService pushNotificationService;
 
     @PostMapping("/users/{targetUserId}")
-    public ResponseEntity<?> pushNotification(@PathVariable long targetUserId) {
+    public ResponseEntity<?> pushNotification(@PathVariable long targetUserId)
+        throws BadRequestException {
         pushNotificationService.sendPickNotification(targetUserId);
 
         return ResponseEntity.ok().build();
