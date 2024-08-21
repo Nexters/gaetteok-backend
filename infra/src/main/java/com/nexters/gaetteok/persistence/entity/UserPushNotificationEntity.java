94db4305407af841d8874b0eb34cea321e7114cd
minkukjo <strike0115@naver.com>
2024-08-22 00:21:15 +0900

    Add index

--- infra/src/main/java/com/nexters/gaetteok/persistence/entity/UserPushNotificationEntity.java
+++ infra/src/main/java/com/nexters/gaetteok/persistence/entity/UserPushNotificationEntity.java
@@ -1,12 +1,22 @@
 package com.nexters.gaetteok.persistence.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user_push_notification")
+@Table(
+    name = "user_push_notification", indexes = {
+    @Index(name = "idx_push_notification_time", columnList = "push_notification_time")
+}
+)
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
