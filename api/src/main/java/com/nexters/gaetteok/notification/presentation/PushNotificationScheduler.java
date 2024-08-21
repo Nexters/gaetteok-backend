a9b351d4ec85af413d065226a4d735ea4f03ee7b
minkukjo <strike0115@naver.com>
2024-08-22 00:19:09 +0900

    Remove comment

--- api/src/main/java/com/nexters/gaetteok/notification/presentation/PushNotificationScheduler.java
+++ api/src/main/java/com/nexters/gaetteok/notification/presentation/PushNotificationScheduler.java
@@ -2,11 +2,13 @@ package com.nexters.gaetteok.notification.presentation;
 
 import com.nexters.gaetteok.firebase.service.PushNotificationService;
 import com.nexters.gaetteok.user.application.UserApplication;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
@@ -18,17 +20,16 @@ public class PushNotificationScheduler {
     private final PushNotificationService pushNotificationService;
     private final UserApplication userApplication;
 
-    // 일단 당분간은 주석!
-//    @Scheduled(fixedDelay = 60000)
-//    public void run() {
-//        log.info("스케쥴러 동작 시작");
-//        LocalDateTime now = LocalDateTime.now();
-//        int targetMinute = (now.getHour() * 60) + now.getMinute();
-//        userApplication.getPushNotificationByMinute(targetMinute).forEach(
-//            pushNotification -> {
-//                log.info("{} 동작 시작", pushNotification.getUserId());
-//                pushNotificationService.sendEncouragementNotification(pushNotification.getUserId());
-//            }
-//        );
-//    }
+    @Scheduled(fixedDelay = 60000)
+    public void run() {
+        log.info("스케쥴러 동작 시작");
+        LocalDateTime now = LocalDateTime.now();
+        int targetMinute = (now.getHour() * 60) + now.getMinute();
+        userApplication.getPushNotificationByMinute(targetMinute).forEach(
+            pushNotification -> {
+                log.info("{} 동작 시작", pushNotification.getUserId());
+                pushNotificationService.sendEncouragementNotification(pushNotification.getUserId());
+            }
+        );
+    }
 }
