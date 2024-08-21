af80b5ac5a81b72346859792d617787671eb66c5
Harry <43809168+minkukjo@users.noreply.github.com>
2024-08-22 00:35:58 +0900

    Add push notification (#31)
    * Add push notification
    
    * Add scheduler
    
    * Fix push notification api path
    
    * Remove comment
    
    * Add index
    
    * Remove log
    
    * Remove log
    
    * Check device token
    
    * Add try catch
    
    * Add method signature

---
+++ api/src/main/java/com/nexters/gaetteok/notification/presentation/PushNotificationScheduler.java
@@ -0,0 +1,39 @@
+package com.nexters.gaetteok.notification.presentation;
+
+import com.nexters.gaetteok.firebase.service.PushNotificationService;
+import com.nexters.gaetteok.user.application.UserApplication;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+@RequiredArgsConstructor
+@Slf4j
+public class PushNotificationScheduler {
+
+    private final PushNotificationService pushNotificationService;
+    private final UserApplication userApplication;
+
+    @Scheduled(fixedDelay = 60000)
+    public void run() {
+        LocalDateTime now = LocalDateTime.now();
+        int targetMinute = (now.getHour() * 60) + now.getMinute();
+        userApplication.getPushNotificationByMinute(targetMinute).forEach(
+            pushNotification -> {
+                try {
+                    pushNotificationService.sendEncouragementNotification(
+                        pushNotification.getUserId());
+                } catch (BadRequestException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        );
+    }
+}
