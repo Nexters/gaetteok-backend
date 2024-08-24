96cca12c678f9e4b81c911c322c0795a99cc40cf
minkukjo <strike0115@naver.com>
2024-08-24 11:09:05 +0900

    Fix guide

--- api/src/main/java/com/nexters/gaetteok/user/presentation/UserPushNotificationSpecification.java
+++ api/src/main/java/com/nexters/gaetteok/user/presentation/UserPushNotificationSpecification.java
@@ -37,7 +37,7 @@ public interface UserPushNotificationSpecification {
         )
     )
     ResponseEntity<GetUserPushNotificationResponse> update(
-        @Parameter(description = "알람시간. MMDD 형태의 숫자값", example = "0830") Integer pushNotificationTime,
+        @Parameter(description = "알람시간. 시/분을 분으로 환산한 값", example = "540은 아침 9시") Integer pushNotificationTime,
         @Parameter(hidden = true) UserInfo userInfo
     );
 
